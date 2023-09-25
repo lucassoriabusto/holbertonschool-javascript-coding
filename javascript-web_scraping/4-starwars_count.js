ab5dec8019d129081ca0ae08a37289d2385159e3
lucassoriabusto <lucassoriabusto@hotmail.com>
2023-09-24 18:12:35 -0700

    Task 04

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -2,6 +2,7 @@
 
 const request = require('request');
 const apiBaseUrl = process.argv[2];
+const characterId = "18";
 
 request(apiBaseUrl, (error, response, body) => {
   if (error) {
@@ -9,10 +10,19 @@ request(apiBaseUrl, (error, response, body) => {
   } else {
     const apiData = JSON.parse(body);
 
-    const moviesWithWedgeAntilles = apiData.results.filter(movie => {
-      return movie.characters.includes("https://swapi-api.hbtn.io/api/people/18/");
-    });
+    let count = 0;
 
-    console.log(moviesWithWedgeAntilles.length);
+    // Recorre todas las películas en la respuesta
+    for (const movie of apiData.results) {
+      // Recorre todos los personajes de la película
+      for (const characterUrl of movie.characters) {
+        // Verifica si el personaje con ID 18 está presente en la película
+        if (characterUrl.includes(`/people/${characterId}/`)) {
+          count++;
+        }
+      }
+    }
+    // Imprime el número de películas en las que aparece "Wedge Antilles"
+    console.log(count);
   }
 });
